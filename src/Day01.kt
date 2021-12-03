1e3996db97453cf7a3da3df72ed3f08295eaae7c
Matthew Cheung <matthew@geneious.com>
2021-12-04 08:56:56 +1300

    Redo Day01 using kotlin.collections.windowed
    This is less lines of code and much nicer :)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,42 +1,21 @@
-import java.util.*
-
 fun main() {
-    fun part1(input: List<String>): Int {
-        var count = 0
-        var previous = Integer.MAX_VALUE
 
-        for (line in input) {
-            val value = line.toInt()
-            if(value > previous) {
-                count++
-            }
-            previous = value
-        }
-        return count
+    fun part1(input: List<String>): Int {
+        return countNumberOfIncreases(getInputAsInts(input))
     }
 
     fun part2(input: List<String>): Int {
-        val window = LinkedList<Int>()
-
-        var count = 0
-        var previous = Int.MAX_VALUE
-        for (line in input) {
-            val newValue = line.toInt()
-            window.addLast(newValue)
-            if (window.size >= 3) {
-                val newSum = window.stream().reduce { a: Int, b: Int -> Integer.sum(a, b) }.orElse(0)
-                if (newSum > previous) {
-                    count++
-                }
-                previous = newSum
-                window.removeFirst()
-            }
-        }
-
-        return count
+        val slidingWindowSums = getInputAsInts(input).windowed(3).map { list -> list.sum() }
+        return countNumberOfIncreases(slidingWindowSums)
     }
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
+fun getInputAsInts(input: List<String>) = input.map { s -> s.toInt() }
+
+fun countNumberOfIncreases(list: List<Int>) = list
+    .windowed(2)
+    .count { (previous, current) -> current > previous }
